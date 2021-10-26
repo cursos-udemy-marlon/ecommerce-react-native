f81ee800f58df34dc345f5de940f5025dc52e66e
Marlon Falcon <marlonfalcon@MacBook-Pro-de-Marlon.local>
2021-10-26 15:15:27 +0200

    36: LLamada a la api de registro de usuario validado con yup

--- ecommerce-app/src/components/Auth/RegisterForm.js
+++ ecommerce-app/src/components/Auth/RegisterForm.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 import { TextInput, Button } from "react-native-paper";
-import { formStyles } from "../../styles";
 import { useFormik } from "formik";
 import * as Yup from "yup";
+import { registerApi } from "../../api/user";
+import { formStyles } from "../../styles";
 
 const RegisterForm = (props) => {
     const { changeForm } = props;
@@ -11,9 +12,13 @@ const RegisterForm = (props) => {
     const formik = useFormik({
         initialValues: initialValues(),
         validationSchema: Yup.object(validationSchema()),
-        onSubmit: (formData) => {
-            console.log("Registro de Usuario enviado")
-            console.log(formData)
+        onSubmit: async (formData) => {
+            try {
+                await registerApi(formData);
+                console.log("OK")
+            } catch (error) {
+                console.log(error);
+            }
         }
     })
 
---
+++ ecommerce-app/src/utils/constants.js
@@ -0,0 +1,1 @@
+export const API_URL = 'http://157.230.188.76:8080';
