748c58fba1c00108d129913eb8f009bece9e3873
Marlon Falcon <marlonfalcon@MacBook-Pro-de-Marlon.local>
2021-10-26 12:46:42 +0200

    31

--- ecommerce-app/App.js
+++ ecommerce-app/App.js
@@ -1,13 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider as PaperProvider } from 'react-native-paper';
+import { Button } from 'react-native-paper';
+
 
 export default function App() {
   return (
+    <PaperProvider>
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+            <Button icon="camera" mode="contained" onPress={() => console.log('Pressed')}>
+              Press me
+            </Button>
     </View>
+    </PaperProvider>
   );
 }
 
