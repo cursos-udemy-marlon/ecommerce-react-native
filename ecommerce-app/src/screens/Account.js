cadcb7655233e06a7c31ca7066bb2a00eaf6b77f
Marlon Falcon <marlonfalcon@MacBook-Pro-de-Marlon.local>
2021-10-27 15:37:00 +0200

    49 Buscador

--- ecommerce-app/src/screens/Account.js
+++ ecommerce-app/src/screens/Account.js
@@ -1,20 +1,23 @@
 import React from 'react'
-import { StyleSheet, View, Text } from 'react-native'
+import { StyleSheet, ScrollView, Text } from 'react-native'
+import Search from "../components/Search"
+import StatusBar from '../components/StatusBar'
+import colors from '../styles/colors'
 
 const Account = () => {
     return (
-        <View style={styles.container}>
-            <Text>Estamos en mi cuenta</Text>
-        </View>
+        <>
+            <StatusBar  backgroundColor={colors.bgDark}
+                barStyle="light-content"
+            />
+            <Search/>
+            <ScrollView>
+                
+                <Text>Estamos en mi cuenta</Text>
+            </ScrollView>
+        </>
     )
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    }
-})
 
 export default Account
