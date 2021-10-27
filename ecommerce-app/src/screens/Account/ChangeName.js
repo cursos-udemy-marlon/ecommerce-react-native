968a58e9e57428ca7bec54a6b4f8ec563599170e
Marlon Falcon <marlonfalcon@MacBook-Pro-de-Marlon.local>
2021-10-28 01:12:06 +0200

    60

--- ecommerce-app/src/screens/Account/ChangeName.js
+++ ecommerce-app/src/screens/Account/ChangeName.js
@@ -1,17 +1,45 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
 import { StyleSheet,  View, Text } from 'react-native';
 import { TextInput, Button } from "react-native-paper";
 import { formStyles } from "../../styles";
 import { useFormik } from "formik";
 import * as Yup from "yup";
+import Toast from "react-native-root-toast";
+import { useFocusEffect, useNavigation } from "@react-navigation/native"
+import { getMeApi, updateUserApi } from "../../api/user"
+import useAuth from "../../hooks/useAuth";
 
 const ChangeName = () => {
+    const { auth } = useAuth();
+    const [loading, setLoading] = useState(false);
+    const navigation = useNavigation();
+
+    useFocusEffect(
+        useCallback( () => {
+            ( async () => {
+                const response = await getMeApi(auth.token)
+                
+                response.name && await formik.setFieldValue("name", response.name);
+                response.lastname && await formik.setFieldValue("lastname", response.lastname);
+            })()
+        }, [] )
+    )
+
     const formik = useFormik({
         initialValues: initialValues(),
         validationSchema: Yup.object(validationSchema()),
-        onSubmit: (formValue) =>{
-            console.log("Formulario Enviado");
-            console.log(formValue);
+        onSubmit: async (formData) =>{
+            setLoading(true);
+            try {
+                await updateUserApi(auth, formData)
+                navigation.goBack();
+            } catch (error) {
+                Toast.show("Error al actualizar los datos",{
+                    position: Toast.position.CENTER,
+                })
+                setLoading(false)
+            }
+            
         }
     })
     return (
@@ -34,6 +62,7 @@ const ChangeName = () => {
             mode="contained"
             style={formStyles.btnSucces}
             onPress={formik.handleSubmit}
+            loading={loading}
            >
                Cambiar nombre y apellidos
            </Button>
